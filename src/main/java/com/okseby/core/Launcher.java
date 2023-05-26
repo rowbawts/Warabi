5c9ef20e6b4137fd101d5d115168dab890202510
okseby <okseby@protonmail.com>
2023-05-26 12:11:15 -0400

    Add WindowManager class, get window drawing to screen and create all capabilities. this is the rest of the previous commit because im dumb

--- src/main/java/com/okseby/core/Launcher.java
+++ src/main/java/com/okseby/core/Launcher.java
@@ -1,9 +1,17 @@
-package com.okseby.Main;
+package com.okseby.core;
 
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("LWJGL Version: " + Version.getVersion());
+
+        WindowManager window = new WindowManager("Warabi Engine", 1600, 900, false);
+        window.init();
+
+        while (!window.windowShouldClose())
+            window.update();
+
+        window.cleanup();
     }
 }
