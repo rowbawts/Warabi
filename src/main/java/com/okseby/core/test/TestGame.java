1aa8033b5a831a6e947a080b458daf1d97ff70d8
okseby <okseby@protonmail.com>
2023-05-31 21:54:06 -0400

    Add chris.

--- src/main/java/com/okseby/core/test/TestGame.java
+++ src/main/java/com/okseby/core/test/TestGame.java
@@ -33,7 +33,7 @@ public class TestGame implements ILogic {
         renderer.init();
 
         Model model = loader.loadOBJModel("/models/cube.obj");
-        model.setTexture(new Texture(loader.loadTexture("/textures/bryan.jpg")));
+        model.setTexture(new Texture(loader.loadTexture("/textures/chris.png")));
 
         entity = new Entity(model, new Vector3f(0, 0, -5), new Vector3f(0, 0, 0), 1);
     }
